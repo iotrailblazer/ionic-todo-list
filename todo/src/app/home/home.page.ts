fcfb65ec05d527ee278675e72969ac02e5a6f6b3
Quan <iotrailblazer@gmail.com>
2021-04-12 14:47:10 -0500

    home page UI

--- todo/src/app/home/home.page.ts
+++ todo/src/app/home/home.page.ts
@@ -6,7 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  todoList = [{
+    itemName : 'Coding',
+    itemDueDate : '13-10-21',
+    itemPriority : 'high',
+    itemCategory : 'word'
+  },
+  {
+    itemName : 'Design',
+    itemDueDate : '28-10-21',
+    itemPriority : 'low',
+    itemCategory : 'work'
+  },
+  {
+    itemName : 'Shopping',
+    itemDueDate : '30-10-21',
+    itemPriority : 'middle',
+    itemCategory : 'personal'
+  },
+  {
+    itemName : 'Workout',
+    itemDueDate : '25-10-21',
+    itemPriority : 'high',
+    itemCategory : 'personal'
+  },
+]
 
+today: number = Date.now();
   constructor() {}
-
 }
